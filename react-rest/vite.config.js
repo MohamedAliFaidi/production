f88f82c4717f9e4bb4a861d14f8b50dc71824f68
MohamedAliFaidi <dalyyy3259@protonmail.com>
2024-02-13 13:30:21 +0100

    fix login routing

--- react-rest/vite.config.js
+++ react-rest/vite.config.js
@@ -14,27 +14,87 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          console.log(id)
           // Split third-party dependencies into smaller chunks
           if (id.includes('node_modules')) {
             // Split common React-related dependencies
+            
+            if (
+              id.includes('@motionone') 
+            ) {
+              return '@motionone_vendor';
+            }
+            if (
+              id.includes('axios') 
+            ) {
+              return 'axios_vendor';
+            }
             if (
               id.includes('@material-tailwind') 
             ) {
-              return '@material-tailwind';
+              if (
+                id.includes('utils') 
+                ) {
+                return '@material-tailwind__utils_vendor';
+                }
+                if (
+                  id.includes('types') 
+                  ) {
+                  return '@material-tailwind__types_vendor';
+                  }
+
+                  if (
+                    id.includes('components') 
+                    ) {
+                    return '@material-tailwind__components_vendor';
+                    }
+                    if (
+                      id.includes('theme') 
+                      ) {
+                      return '@material-tailwind__theme_vendor';
+                      }
+              
             }
             if (
               id.includes('react-router-dom') ||
               id.includes('@remix-run') ||
               id.includes('react-router')
             ) {
-              return '@react-router';
+              return '@react-router_vendor';
             }
-            if (id.includes('@heroicons/react') || id.includes('@material-tailwind/react') || id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom') || id.includes('react-hot-toast') || id.includes('zustand')) {
+            if (id.includes('@heroicons')) {
+              return '@heroicons_vendor';
+            }
+            if ( id.includes('react-dom')  || id.includes('react-hot-toast') || id.includes('zustand')) {
+              return 'react-dom_vendor';
+            }
+            if (  id.includes('react-hot-toast') ) {
+              return 'react-hot-toast_vendor';
+            }
+            if (  id.includes('use-sync-external-store') ) {
+              console.log(id)
+
+              return 'use-sync-external-store_vendor';
+            }
+            if (  id.includes('@floating-ui')  ) {
+              console.log(id)
+
+              return ' @floating-ui_vendor';
+            }
+           
+            if (  id.includes('react') && !id.includes('@material-tailwind') ) {
+              console.log(id)
+
               return 'react_vendor';
             }
+
+
+
+
+            
+        
             // Split other third-party dependencies
             else {
+
               return 'other_vendor';
             }
           }
